de4587bf6b2d77a27cc018798b301beb78d16906
Oana Vezentan <oana.vezentan@gmail.com>
2017-11-07 17:46:50 +0100

    Added schemas in spec

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: any[];
+  hasPermission: boolean = false;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers()
+        .then(users => this.users = users)
+      .catch(e => console.log(e.message))
+    } else {
+      this.users = [];
+    }
   }
 
+  async getUsers() {
+    return [
+      {name: 'john', email: 'john@example.com'},
+      {name: 'mike', email: 'mike@example.com'}
+    ];
+  }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
